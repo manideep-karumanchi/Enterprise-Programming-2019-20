bc10f7a3192d5e09cf552b2741146f3c4a4d62da
manideep-karumanchi <manideep.karumanchi@gmail.com>
2019-07-10 08:38:32 +0530

    Add files via upload

---
+++ TempConverter/TempConverter/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TempConverter.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Fahrenheit: <input type="text" id="ftb" runat="server"/> to
+        <input id="crb" name="totemp" runat="server" type="radio" value="Celsius" /> Celsius&nbsp;
+        <input id="krb" name="totemp" runat="server" type="radio" value="Kelvin" /> Kelvin<br />
+        <input type="submit" value="Convert" id="cb" runat="server" onserverclick="cb_ServerClick"/>
+        <p runat="server" id="output"></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TempConverter/TempConverter/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TempConverter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cb_ServerClick(object sender, EventArgs e)
+        {
+            double ft;
+            bool c = double.TryParse(ftb.Value,out ft);
+            if (c)
+            {
+                double ct = 0, kt = 0;
+
+                if (crb.Checked)
+                {
+                    ct = ftoc(ft);
+                    output.InnerText = ft + " Fahrenheit Temp = " + ct + " Celsius Temp";
+                }
+                if (krb.Checked)
+                {
+                    kt = ftok(ft);
+                    output.InnerText = ft + " Fahrenheit Temp = " + kt + " Celsius Temp";
+                }
+            }
+            else {
+                output.InnerText = "Unable to parse the given value. Please provide a number in the input textbox";
+            }
+        }
+        double ftoc(double ft) {
+            return (ft - 32) * 5 / 9;
+        }
+        double ftok(double ft)
+        {
+            return (ft - 32) * (5 / 9)+273;
+        }
+        
+    }
+}
