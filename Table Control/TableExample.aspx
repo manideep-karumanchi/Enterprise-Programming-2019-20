a54670f3f35baa7310822480b2e11b8743c916a3
manideep-karumanchi <manideep.karumanchi@gmail.com>
2019-07-18 10:12:43 +0530

    Add files via upload

---
+++ Table Control/TableExample.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TableExample.aspx.cs" Inherits="ListControls.TableExample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        No. Of Rows: <asp:TextBox ID="txtrows" runat="server"></asp:TextBox><br /> 
+        No. Of Cols: <asp:TextBox ID="txtcols" runat="server"></asp:TextBox><br />
+        <asp:CheckBox ID="chkborder" runat="server" Text="Put Border" />    <br />
+        <asp:Button ID="cmdgt" runat="server" Text="Generate Table" OnClick="cmdgt_Click" /><br />
+        <asp:Table ID="tbl" runat="server"></asp:Table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Table Control/TableExample.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ListControls
+{
+    public partial class TableExample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+                tbl.BorderStyle = BorderStyle.None;
+        }
+
+        protected void cmdgt_Click(object sender, EventArgs e)
+        {
+            tbl.Controls.Clear();
+
+            int rows = Int32.Parse(txtrows.Text);
+            int cols = Int32.Parse(txtcols.Text);
+
+            for (int r = 0; r < rows; r++) {
+                TableRow  tr = new TableRow();
+                for (int c = 0; c < cols; c++) {
+                    TableCell tc = new TableCell();
+                    tc.Text = "Table Cell(" + (r + 1) + "," + (c + 1) + ")";
+                    tr.Controls.Add(tc);
+                }
+                tbl.Controls.Add(tr);
+            }
+            if (chkborder.Checked) {
+                tbl.BorderStyle = BorderStyle.Inset;
+            }
+        }
+    }
+}
