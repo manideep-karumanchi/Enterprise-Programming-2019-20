2735ef03a635ca66515c7f0000d6c1629244e6f3
manideep-karumanchi <manideep.karumanchi@gmail.com>
2019-09-08 13:42:56 +0530

    Add files via upload

---
+++ AdoExample/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AdoExample.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="border:solid; border-width:medium;background-color:#9bdbe4">
+        <br />Select Author:
+        <asp:DropDownList ID="lstAuthors" runat="server" AutoPostBack="True" OnSelectedIndexChanged="lstAuthors_SelectedIndexChanged">
+        </asp:DropDownList> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <asp:Button ID="cmdub" runat="server" Text="Update" OnClick="cmdub_Click" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <asp:Button ID="cmddb" runat="server" Text="Delete" OnClick="cmddb_Click" />
+        <br />
+        Or:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <asp:Button ID="cmdcb" runat="server" Text="Create New" OnClick="cmdcb_Click" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <asp:Button ID="cmdin" runat="server" Text="Insert New" OnClick="cmdin_Click" />
+        
+    </div>
+    <br /><br /><br />
+    <div style="border:solid; border-width:medium;background-color:#bfb565">
+        Unique Id:<asp:TextBox ID="txtid" runat="server"></asp:TextBox><br />
+        First Name:<asp:TextBox ID="txtfn" runat="server"></asp:TextBox><br />
+        Last Name:<asp:TextBox ID="txtln" runat="server"></asp:TextBox><br />
+        Phone:<asp:TextBox ID="txtphone" runat="server"></asp:TextBox><br />
+        Address:<asp:TextBox ID="txtadd" runat="server"></asp:TextBox><br />
+        City:<asp:TextBox ID="txtcity" runat="server"></asp:TextBox><br />
+        State:<asp:TextBox ID="txtstate" runat="server"></asp:TextBox><br />
+        Zip-Code:<asp:TextBox ID="txtzip" runat="server"></asp:TextBox><br />
+        Contract:<asp:CheckBox ID="chkcontract" runat="server" />
+        <br />
+        <asp:Label ID="lbldetails" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ AdoExample/Default.aspx.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdoExample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                fillauthors();
+            }
+        }
+        public void fillauthors(){
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS; Initial Catalog=pubs; User Id=sa; Password=sqlserver;");
+            SqlCommand cmd = new SqlCommand("Select * from authors", con);
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read()) {
+                    ListItem li = new ListItem(reader["au_fname"]+" "+reader["au_lname"],reader["au_id"].ToString());
+                    lstAuthors.Items.Add(li);
+                }
+            }
+            catch (Exception ex)
+            {
+                lbldetails.Text = "<b>Error Occured:" + ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        protected void lstAuthors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS; Initial Catalog=pubs; User Id=sa; Password=sqlserver;");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Select * from authors where au_id='"+lstAuthors.SelectedValue+"'";
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                txtid.Text = reader["au_id"].ToString();
+                txtfn.Text = reader["au_fname"].ToString();
+                txtln.Text = reader["au_lname"].ToString();
+                txtphone.Text = reader["phone"].ToString();
+                txtadd.Text = reader["address"].ToString();
+                txtcity.Text = reader["city"].ToString();
+                txtstate.Text = reader["state"].ToString();
+                txtzip.Text = reader["zip"].ToString();
+                chkcontract.Checked = (bool)reader["contract"];
+            }
+            catch (Exception ex) {
+                lbldetails.Text = "<b>Error Occured:" + ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void cmdcb_Click(object sender, EventArgs e)
+        {
+            txtid.Text = "";
+            txtfn.Text = "";
+            txtln.Text = "";
+            txtphone.Text = "";
+            txtadd.Text = "";
+            txtcity.Text = "";
+            txtstate.Text = "";
+            txtzip.Text = string.Empty;
+            chkcontract.Checked = false;
+        }
+
+        protected void cmdin_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text != "" && txtfn.Text != "" && txtln.Text != "" && txtphone.Text != "" && txtzip.Text != "")
+            {
+                string query = "insert into authors (au_id,au_fname,au_lname,phone,address,city,state,zip,contract) values('" + txtid.Text + "', '" + txtfn.Text + "', '" + txtln.Text + "', '" + txtphone.Text + "', '" + txtadd.Text + "', '" + txtcity.Text + "', '" + txtstate.Text + "', '" + txtzip.Text + "', " + Convert.ToInt16(chkcontract.Checked)+")";
+                SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS; Initial Catalog=pubs; User Id=sa; Password=sqlserver;");
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = query;
+                try
+                {
+                    con.Open();
+                    int res = cmd.ExecuteNonQuery();
+                    if (res != 1)
+                    {
+                        lbldetails.Text = "<b>Insertion Failed.</b>";
+                    }
+                    else {
+                        lbldetails.Text = "<b>Record Inserted.</b>";
+                        fillauthors();
+                        Response.Redirect("Default.aspx");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lbldetails.Text = "<b>Error Occured:" + ex.Message;
+                }
+                finally
+                {
+                    con.Close();
+                }   
+
+            }
+            else {
+                lbldetails.Text = "Some of the mandatory textboxes contains null values.";
+            }
+        }
+
+        protected void cmdub_Click(object sender, EventArgs e)
+        {
+            string query = "update authors set au_fname=@au_fname, au_lname=@au_lname, phone=@phone, address=@address, city=@city, state=@state, zip=@zip, contract=@contract where au_id=@au_id";
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS; Initial Catalog=pubs; User Id=sa; Password=sqlserver;");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = query;
+            try
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@au_fname", txtfn.Text);
+                cmd.Parameters.AddWithValue("@au_lname", txtln.Text);
+                cmd.Parameters.AddWithValue("@phone", txtphone.Text);
+                cmd.Parameters.AddWithValue("@address", txtadd.Text);
+                cmd.Parameters.AddWithValue("@city", txtcity.Text);
+                cmd.Parameters.AddWithValue("@state", txtstate.Text);
+                cmd.Parameters.AddWithValue("@zip", txtzip.Text);
+                cmd.Parameters.AddWithValue("@contract", Convert.ToInt16(chkcontract.Checked));
+                cmd.Parameters.AddWithValue("@au_id", txtid.Text);
+                int res = cmd.ExecuteNonQuery();
+                if (res != 1)
+                {
+                    lbldetails.Text = "<b>Insertion Failed.</b>";
+                }
+                else
+                {
+                    lbldetails.Text = "<b>Record Inserted.</b>";
+                    fillauthors();
+                    Response.Redirect("Default.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                lbldetails.Text = "<b>Error Occured:" + ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }   
+
+        }
+
+        protected void cmddb_Click(object sender, EventArgs e)
+        {
+            string query = "delete from authors where au_id=@au_id";
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS; Initial Catalog=pubs; User Id=sa; Password=sqlserver;");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = query;
+            try
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@au_id", txtid.Text);
+                int res = cmd.ExecuteNonQuery();
+                if (res != 1)
+                {
+                    lbldetails.Text = "<b>Insertion Failed.</b>";
+                }
+                else
+                {
+                    lbldetails.Text = "<b>Record Inserted.</b>";
+                    fillauthors();
+                    Response.Redirect("Default.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                lbldetails.Text = "<b>Error Occured:" + ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }   
+        }
+    }
+}
