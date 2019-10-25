ed1405878af8def5a02111902ba3d0d8060991bc
manideep-karumanchi <manideep.karumanchi@gmail.com>
2019-10-24 20:41:38 -0700

    Add files via upload

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            ViewBag.Greetings=DateTime.Now.Hour>=12?"Good Afternoon":"Good Morning";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RVSPForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RVSPForm(GuestResponse guestRespone) {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestRespone);
+            }
+            else {
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please Enter your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter your Contact Number")]
+        public string ContactNumber { set; get; }
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Please Enter Email in Correct Format")]
+        public string Email { set; get; }
+        [Required(ErrorMessage = "Choose your Option")]
+        public bool? AttendingParty { set; get; }
+    }
+}
